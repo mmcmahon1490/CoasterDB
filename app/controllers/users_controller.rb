1261924f6ddc8884db3b343b5b3bfd96ab1d02f7
Michael McMahon <mmcmahon1490@gmail.com>
2020-10-20 18:26:51 -0400

    Backend Working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,18 @@ class UsersController < ApplicationController
   end
 
   # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    def create
+      @user = User.new(user_params)
+      
+      if @user.save
+        @token = encode({id: @user.id})
+        render json: {
+          user: @user.attributes.except(:password_digest),
+          token: @token
+          }, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
   end
 
   # PATCH/PUT /users/1
@@ -46,6 +50,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :email, :password_digest)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coaster.destroy_all
+Park.destroy_all
+User.destroy_all
+
+@user = User.create(username: 'admin', email: 'admin@email.com', password: '123456')
+@cedarpoint = Park.create(name: 'Cedar Point', location: 'Sandusky, Ohio')
+@bluestreak = Coaster.create(name: 'Blue Streak', park:@cedarpoint)
+@ccmr = Coaster.create(name: 'Cedar Creek Mine Ride', park:@cedarpoint)
+@corkscrew = Coaster.create(name: 'Corkscrew', park:@cedarpoint)
+@gatekeeper = Coaster.create(name: 'GateKeeper', park:@cedarpoint)
+@gemini = Coaster.create(name: 'Gemini', park:@cedarpoint)
+@irondragon = Coaster.create(name: 'Iron Dragon', park:@cedarpoint)
+@magnum = Coaster.create(name: 'Magnum XL-200', park:@cedarpoint)
+@maverick = Coaster.create(name: 'Maverick', park:@cedarpoint)
+@millenniumforce = Coaster.create(name: 'Millennium Force', park:@cedarpoint)
+@raptor = Coaster.create(name: 'Raptor', park:@cedarpoint)
+@rougarou = Coaster.create(name: 'Rougarou', park:@cedarpoint)
+@steelvengeance = Coaster.create(name: 'Steel Vengeance', park:@cedarpoint)
+@topthrilldragster = Coaster.create(name: 'Top Thrill Dragster', park:@cedarpoint)
+@valravn = Coaster.create(name: 'Valravn', park:@cedarpoint)
+@wickedtwister = Coaster.create(name: 'Wicked Twister', park:@cedarpoint)
+@wildernessrun = Coaster.create(name: 'Wilderness Run', park:@cedarpoint)
+@woodstockexpress = Coaster.create(name: 'Woodstock Express', park:@cedarpoint)
+@bluestreak = Coaster.create(name: 'Blue Streak', park:@cedarpoint)
+
+puts "#{Park.count} parks created"
+puts "#{Coaster.count} coasters created"
