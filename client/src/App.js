a80cd0e24b835dd352f1471b4e5ff63953d9741c
Michael McMahon <mmcmahon1490@gmail.com>
2020-10-20 18:26:23 -0400

    Backend Working

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './screens/Home/Home'
+import Login from'./screens/Login/Login'
 
 function App() {
+
+  
+
   return (
     <div className="App">
-      <Home />
+      <Switch>
+        <Route path='/Home' />
+        <Route path='/login' />
+          
+      </Switch>
+      
     </div>
   );
 }
