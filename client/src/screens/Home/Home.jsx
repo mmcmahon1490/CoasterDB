f096f592ad93aa286431cbf45ae1942edc28115c
Michael McMahon <mmcmahon1490@gmail.com>
2020-10-20 11:58:03 -0400

    Home Screen

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import HomeLayout from '../../components/shared/HomeLayout/HomeLayout';
-
+import './Home.css'
 
 const Home = () => {
   return (
     <HomeLayout>
-    <div className='home'>
-      <h1>Go Cats</h1>
-      </div>
-      </HomeLayout>
+    
+    </HomeLayout>
     
   )
 }
