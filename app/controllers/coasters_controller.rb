18ff4f45d87e3de57eb361804efbf37c5c42e0bc
Michael McMahon <mmcmahon1490@gmail.com>
2020-10-21 16:00:59 -0400

    Fixing Seed

--- app/controllers/coasters_controller.rb
+++ app/controllers/coasters_controller.rb
@@ -1,5 +1,5 @@
 class CoastersController < ApplicationController
-  # before_action :set_coaster, only: [:show, :update, :destroy]
+  before_action :set_coaster, only: [:show, :update, :destroy]
   # before_action :authorize_request, only: [:create, :update, :destroy]
 
   # GET /coasters
