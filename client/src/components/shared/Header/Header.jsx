ccda3ab90444906cea48eacdc2272cf6f24c8a24
Michael McMahon <mmcmahon1490@gmail.com>
2020-10-25 12:56:05 -0400

    Working on login/logout

--- client/src/components/shared/Header/Header.jsx
+++ client/src/components/shared/Header/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
+  const { currentUser, handleLogout } = props;
   return (
   
     <div className='title-card'>
       <h1>roller coaster db</h1>
       <Link to='/home'>roller coaster db</Link>
+      {
+        currentUser ?
+          <>
+            <p>{currentUser.username}</p>
+            <button onClick={handleLogout}>logout</button>
+          </>
+          :
+          <Link to='/login'>login/register</Link>
+      }
     <h4>space eater</h4>
     
       <Link to='/Login'>login</Link>
